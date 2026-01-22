23ef22f4515f12283ff5054d03cbcde8340d58e6
Angelo <angelofigueroaflores10@gmail.com>
2026-01-22 17:20:17 -0300

    Segundo commit

--- ProyectoVyPpro/urls.py
+++ ProyectoVyPpro/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for ProyectoVypro project.
+URL configuration for ProyectoVyPpro project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
@@ -18,15 +18,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings 
-from VyPro.views import pagina_no_encontrada, error_servidor
+from VyPpro.views import pagina_no_encontrada, error_servidor
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('VyPro.urls')),
+    path('',include('VyPpro.urls')),
 ]
 
 if settings.DEBUG:
     urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-handler404 = 'VyPro.views.pagina_no_encontrada'
-handler500 = 'VyPro.views.error_servidor'
+handler404 = 'VyPpro.views.pagina_no_encontrada'
+handler500 = 'VyPpro.views.error_servidor'
